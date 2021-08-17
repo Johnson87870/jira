7f9a01f8754a35a65b9c1b82820c3eb217ba5f6b
Johnson Wu <wj_johnson9581314@163.com>
2021-08-17 15:12:37 +0800

    chroe 安装devtool

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import { ProjectListScreens } from "screens/project-list";
+import { LoginScreen } from "screens/login";
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreens />
+      {/* <ProjectListScreens /> */}
+      <LoginScreen />
     </div>
   );
 }
